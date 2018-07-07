d820fc9d18d27b5415afd2b336f0544b5ba807d1
Jorgelu1s <mejiajimenez@gmail.com>
2018-07-07 13:09:15 -0500

    Added vuex-persist to persist the state

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
 
 Vue.use(Vuex)
 
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex',
+  storage: window.localStorage
+})
+
 export default new Vuex.Store({
+  plugins: [vuexLocalStorage.plugin],
+
   state: {
     customers: [],
     groups: [],
